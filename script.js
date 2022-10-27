3621b250f3b33782b142b0c38d952e6b9c0bb07f
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2022-10-27 06:02:43 +0000

    remove: page loader

--- script.js
+++ script.js
@@ -10,13 +10,6 @@ const tabTitle = document.querySelector("#tab-title");
 const title = document.querySelector("#title");
 const cover = document.querySelector("#cover");
 
-// page loader
-window.addEventListener("load", () => {
-  document.querySelector(".page-loader").classList.add("slide-out-right");
-  setTimeout(() => {
-    document.querySelector(".page-loader").style.display = "none";
-  }, 10000);
-});
 
 // Bg Animation Effect
 function bgAnimationItems() {
